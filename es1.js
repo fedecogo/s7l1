b891ba9d8bd35fcc7e3649191a5f9dd47fc82460
fedecogo <141748020+fedecogo@users.noreply.github.com>
2023-10-09 14:27:19 +0200

    first commit

---
+++ es1.js
@@ -0,0 +1,43 @@
+class User {
+    constructor(_firstName, _lastName, _age, _location ='no-location-add') {
+      this.firstName = _firstName
+      this.lastName = _lastName
+      this.age = _age
+      this.location = _location
+    }
+    showInfo() {
+      return `Mi chiamo ${this.firstName} ${this.lastName},ho ${this.age} anni e vivo a ${this.location}`
+    }
+  
+  }
+  const durden = new User (
+    'Tyler',
+  'Durden',
+  '30',
+  )
+  const me = new User (
+    'Federico',
+  'Capizzi',
+  '21',
+  'Milano'
+  )
+  const gasby = new User (
+    'Il Grande',
+  'Gasby',
+  '31',
+  'New York'
+  )
+
+  console.log(me.showInfo())
+  console.log(durden.showInfo())
+  console.log(gasby.showInfo())
+
+  function confrontaEta(x, y) {
+    if (x.age > y.age) {
+      console.log(`${x.firstName} ${x.lastName} è più vecchio di ${y.firstName} ${y.lastName}.`);
+    } else {
+      console.log(`${y.firstName} ${y.lastName} è più vecchio di ${x.firstName} ${x.lastName}.`);
+    } 
+  }
+  confrontaEta(durden, gasby);
+  
